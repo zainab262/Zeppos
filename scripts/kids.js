5f97e4b0d769182a942339fbfd945c457c89b113
zainab262 <zainabsddq10@gmail.com>
2025-02-07 20:30:31 +0530

    update

---
+++ scripts/kids.js
@@ -0,0 +1,29 @@
+let url="https://honey-natural-gallium.glitch.me/kids"
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(products => {
+            const container = document.getElementById("product-container");
+
+            products.forEach(product => {
+                const productDiv = document.createElement("div");
+                productDiv.classList.add("product");
+
+                productDiv.innerHTML = `
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p><strong>Category:</strong> ${product.category}</p>
+                    <p><strong>Price:</strong> ${product.price}</p>
+                   
+                    <button>Add to cart</button>
+                        
+                    
+                    
+                `;
+
+                container.appendChild(productDiv);
+            });
+        })
+        .catch(error => console.error("Error loading products:", error));
+});
